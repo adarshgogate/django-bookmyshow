4f70647d72cc45a5024a136fd903366f24fed428
adarshgogate <gogateadarsh@gmail.com>
2026-01-15 17:31:52 +0530

    Fixing the Static Images

--- movies/admin.py
+++ movies/admin.py
@@ -6,7 +6,7 @@ from django.utils.html import format_html
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['name', 'rating', 'genre', 'language', 'trailer_preview','demo_poster'] 
-    fields = ['name', 'image', 'rating', 'cast', 'description', 'genre', 'language', 'trailer_url','demo_poster']
+    fields = ['name', 'rating', 'cast', 'description', 'genre', 'language', 'trailer_url','demo_poster']
     def trailer_preview(self, obj): 
         if obj.embed_url(): 
             return format_html(
